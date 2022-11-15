176d33bd47579ce16b3bd17def0205a75c91b0a8
Đỗ Quang Khánh <do.quang.khanh@sun-asterisk.com>
2022-11-15 14:09:55 +0700

    init project

---
+++ Netflix Movies/Netflix Movies/SceneDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  SceneDelegate.swift
+//  Netflix Movies
+//
+//  Created by Quang Khánh on 15/11/2022.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+}
+
---
+++ Netflix Movies/Netflix Movies/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Netflix Movies
+//
+//  Created by Quang Khánh on 15/11/2022.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
