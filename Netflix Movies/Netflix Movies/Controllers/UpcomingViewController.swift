db43897a7ae6757d2fe05d11840f5d394766352e
Đỗ Quang Khánh <do.quang.khanh@sun-asterisk.com>
2022-11-15 16:03:15 +0700

    Init Project

---
+++ Netflix Movies/Netflix Movies/Controllers/UpcomingViewController.swift
@@ -0,0 +1,17 @@
+//
+//  UpcomingViewController.swift
+//  Netflix Movies
+//
+//  Created by Khanh on 15/11/2022.
+//
+
+import UIKit
+
+final class UpcomingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+    }
+}
